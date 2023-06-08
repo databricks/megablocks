e7b000b6638c8899dd12a666aa972fb9018d558e
Trevor Gale <tgale@stanford.edu>
2023-06-08 16:10:17 +0000

    Updating hparams for dense/dmoe models.

--- exp/gpt2/gpt2_1315m_8gpu.sh
+++ exp/gpt2/gpt2_1315m_8gpu.sh
@@ -37,84 +37,86 @@ TRAINING_ARGUMENTS="\
 --global-batch-size 512 \
 --train-iters ${TRAINING_STEPS} \
 --lr-decay-iters ${TRAINING_STEPS} \
---lr 0.00015 \
---min-lr 0.00001 \
+--lr 0.0004 \
+--min-lr 0.00004 \
 --lr-decay-style cosine \
 --lr-warmup-fraction 0.01 \
 --clip-grad 1.0"
 
 PILE_DATASET="\
 1.0 \
-../pile_gpt2/01_text_document \
+../../pile_gpt2/01_text_document \
 1.0 \
-../pile_gpt2/02_text_document \
+../../pile_gpt2/02_text_document \
 1.0 \
-../pile_gpt2/03_text_document \
+../../pile_gpt2/03_text_document \
 1.0 \
-../pile_gpt2/04_text_document \
+../../pile_gpt2/04_text_document \
 1.0 \
-../pile_gpt2/05_text_document \
+../../pile_gpt2/05_text_document \
 1.0 \
-../pile_gpt2/06_text_document \
+../../pile_gpt2/06_text_document \
 1.0 \
-../pile_gpt2/07_text_document \
+../../pile_gpt2/07_text_document \
 1.0 \
-../pile_gpt2/08_text_document \
+../../pile_gpt2/08_text_document \
 1.0 \
-../pile_gpt2/09_text_document \
+../../pile_gpt2/09_text_document \
 1.0 \
-../pile_gpt2/10_text_document \
+../../pile_gpt2/10_text_document \
 1.0 \
-../pile_gpt2/11_text_document \
+../../pile_gpt2/11_text_document \
 1.0 \
-../pile_gpt2/12_text_document \
+../../pile_gpt2/12_text_document \
 1.0 \
-../pile_gpt2/13_text_document \
+../../pile_gpt2/13_text_document \
 1.0 \
-../pile_gpt2/14_text_document \
+../../pile_gpt2/14_text_document \
 1.0 \
-../pile_gpt2/15_text_document \
+../../pile_gpt2/15_text_document \
 1.0 \
-../pile_gpt2/16_text_document \
+../../pile_gpt2/16_text_document \
 1.0 \
-../pile_gpt2/17_text_document \
+../../pile_gpt2/17_text_document \
 1.0 \
-../pile_gpt2/18_text_document \
+../../pile_gpt2/18_text_document \
 1.0 \
-../pile_gpt2/19_text_document \
+../../pile_gpt2/19_text_document \
 1.0 \
-../pile_gpt2/20_text_document \
+../../pile_gpt2/20_text_document \
 1.0 \
-../pile_gpt2/21_text_document \
+../../pile_gpt2/21_text_document \
 1.0 \
-../pile_gpt2/22_text_document \
+../../pile_gpt2/22_text_document \
 1.0 \
-../pile_gpt2/23_text_document \
+../../pile_gpt2/23_text_document \
 1.0 \
-../pile_gpt2/24_text_document \
+../../pile_gpt2/24_text_document \
 1.0 \
-../pile_gpt2/25_text_document \
+../../pile_gpt2/25_text_document \
 1.0 \
-../pile_gpt2/26_text_document \
+../../pile_gpt2/26_text_document \
 1.0 \
-../pile_gpt2/27_text_document \
+../../pile_gpt2/27_text_document \
 1.0 \
-../pile_gpt2/28_text_document \
+../../pile_gpt2/28_text_document \
 1.0 \
-../pile_gpt2/29_text_document"
+../../pile_gpt2/29_text_document"
 
 # NOTE: We don't train for enough tokens for the
 # split to matter.
 DATA_ARGUMENTS="\
 --data-path ${PILE_DATASET} \
---vocab-file ../gpt2-vocab.json \
---merge-file ../gpt2-merges.txt \
+--vocab-file ../../gpt2-vocab.json \
+--merge-file ../../gpt2-merges.txt \
 --make-vocab-size-divisible-by 1024 \
 --split 969,30,1"
 
 COMPUTE_ARGUMENTS="\
 --fp16 \
---DDP-impl local"
+--DDP-impl local \
+--no-async-tensor-model-parallel-allreduce \
+--use-flash-attn"
 
 CHECKPOINT_ARGUMENTS="\
 --save-interval 2000 \
