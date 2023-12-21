92314d716c33afc1829e3cee904d6708f57dd701
Chuck Tang <chuck.tang@databricks.com>
2023-12-21 03:38:28 +0000

    test mlp benchmarks

--- megablocks/layers/test_mlp_benchmark.py
+++ megablocks/layers/test_mlp_benchmark.py
@@ -2,23 +2,19 @@ import unittest
 
 from absl.testing import parameterized
 from megablocks import benchmark_util
-from megablocks.layers.arguments import Arguments
-from megablocks import ops
 from megablocks.layers import mpu
-from megablocks.layers.mlp import MLP, SparseMLP, GroupedMLP, TransformerEngineMLP, TorchMLP
-import stk
-
+from megablocks.layers.arguments import Arguments
+from megablocks.layers import dmlp_registry
 import torch
 
-def log_benchmark(name, arguments, time, std, flops):
-    benchmark_util.log_benchmark(name, arguments, time, std)
-    print("="*60)
-
-_MATMUL_TESTS = (
-    (4, 2048, 8192, 8192, 32, 8, 4),
+_MLP_TESTS = (
+    ('grouped', 4, 2048, 8192, 8192, 32, 8, 4),
+    ('torch', 4, 2048, 8192, 8192, 32, 8, 4),
+    ('te', 4, 2048, 8192, 8192, 32, 8, 4),
 )
 
 class MLPBenchmark(parameterized.TestCase):
+
     def forwardBackward(self, model, x, forward_only=False, kwargs=None):
         y = model(x, **kwargs) if kwargs else model(x)
         if forward_only: return
@@ -38,100 +34,20 @@ class MLPBenchmark(parameterized.TestCase):
     def get_uniform_tokens_per_expert(self, num_tokens_per_expert, expert_count):
         return torch.Tensor([num_tokens_per_expert for _ in range(expert_count)]).cuda()
     
-    @parameterized.parameters(*_MATMUL_TESTS)
-    def testTorchMLPBenchmark(self, bs, sl, hs, fhs, ne, ws, top_k):
+    @parameterized.parameters(*_MLP_TESTS)
+    def testMLPRuntimeBenchmarks(self, mlp_impl, bs, sl, hs, fhs, ne, ws, top_k):
+        """ Benchmark the runtimes of the MLP forward and backward pass with expert parallelism. """
+
         args = Arguments(
             hidden_size=hs,
             ffn_hidden_size=fhs,
             moe_num_experts=ne,
             moe_top_k=top_k,
-            grouped_mlp=True,
             moe_expert_model_parallelism=True,
+            mlp_impl=mlp_impl,
         )
         arguments = {
-            "batch_size": bs,
-            "sequence_length": sl,
-            "hidden_size": hs,
-            "ffn_hidden_size": fhs,
-            "num_experts": ne,
-            "world_size": ws,
-            "top_k": top_k
-        }
-        # mock the expert parallelism functions
-        mpu.get_expert_parallel_rank = lambda args: 0
-        mpu.get_expert_parallel_world_size = lambda args: ws
-        epr = mpu.experts_per_rank(args)
-        assert epr == ne // ws
-
-        # Generate input and uniform tokens_per_expert
-        num_tokens_per_expert = (bs * sl * top_k * ws) // ne
-        model = TorchMLP(args).to(torch.bfloat16)
-        x = torch.randn(epr * num_tokens_per_expert, hs, device="cuda", dtype=torch.bfloat16)
-        tokens_per_expert = self.get_uniform_tokens_per_expert(num_tokens_per_expert, epr)
-
-        # run benchmark forward backward
-        benchmark = lambda: self.forwardBackward(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=False)
-        mean_t, std_t = benchmark_util.benchmark_function(benchmark)
-        benchmark_util.log_benchmark("1::testTorchForwardBackwardBenchmark", arguments, mean_t, std_t)
-
-        # run benchmark forward
-        benchmark = lambda: self.forwardBackward(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=True)
-        mean_t, std_t = benchmark_util.benchmark_function(benchmark)
-        benchmark_util.log_benchmark("1::testTorchForwardBenchmark", arguments, mean_t, std_t)
-
-    @parameterized.parameters(*_MATMUL_TESTS)
-    def testTransformerEngineMLPBenchmark(self, bs, sl, hs, fhs, ne, ws, top_k):
-        args = Arguments(
-            hidden_size=hs,
-            ffn_hidden_size=fhs,
-            moe_num_experts=ne,
-            moe_top_k=top_k,
-            mlp_impl='te',
-            moe_expert_model_parallelism=True,
-        )
-        arguments = {
-            "batch_size": bs,
-            "sequence_length": sl,
-            "hidden_size": hs,
-            "ffn_hidden_size": fhs,
-            "num_experts": ne,
-            "world_size": ws,
-            "top_k": top_k,
-            "mlp_impl": 'te',
-        }
-        # mock the expert parallelism functions
-        mpu.get_expert_parallel_rank = lambda args: 0
-        mpu.get_expert_parallel_world_size = lambda args: ws
-        epr = mpu.experts_per_rank(args)
-        assert epr == ne // ws
-
-        # Generate input and uniform tokens_per_expert
-        num_tokens_per_expert = (bs * sl * top_k * ws) // ne
-        model = TransformerEngineMLP(args)
-        x = torch.randn(epr * num_tokens_per_expert, hs, device="cuda", dtype=torch.bfloat16)
-        tokens_per_expert = self.get_uniform_tokens_per_expert(num_tokens_per_expert, epr)
-
-        # run benchmark forward backward
-        benchmark = lambda: self.transformerEngineForwardBackward(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=False)
-        mean_t, std_t = benchmark_util.benchmark_function(benchmark)
-        benchmark_util.log_benchmark("2::testTransformerEngineForwardBackwardBenchmark", arguments, mean_t, std_t)
-
-        # run benchmark forward
-        benchmark = lambda: self.transformerEngineForwardBackward(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=True)
-        mean_t, std_t = benchmark_util.benchmark_function(benchmark)
-        benchmark_util.log_benchmark("2::testTransformerEngineForwardBenchmark", arguments, mean_t, std_t)
-
-    @parameterized.parameters(*_MATMUL_TESTS)
-    def testGroupedMLPBenchmark(self, bs, sl, hs, fhs, ne, ws, top_k):
-        args = Arguments(
-            hidden_size=hs,
-            ffn_hidden_size=fhs,
-            moe_num_experts=ne,
-            moe_top_k=top_k,
-            grouped_mlp=True,
-            moe_expert_model_parallelism=True,
-        )
-        arguments = {
+            "mlp_impl": mlp_impl,
             "batch_size": bs,
             "sequence_length": sl,
             "hidden_size": hs,
@@ -140,24 +56,33 @@ class MLPBenchmark(parameterized.TestCase):
             "world_size": ws,
             "top_k": top_k,
         }
-        # mock the expert parallelism functions
+        # Mock the expert parallelism functions
         mpu.get_expert_parallel_rank = lambda args: 0
         mpu.get_expert_parallel_world_size = lambda args: ws
-        epr = mpu.experts_per_rank(args)
-        assert epr == ne // ws
+        experts_per_rank = mpu.experts_per_rank(args)
+        assert experts_per_rank == ne // ws
 
-        # Generate input and uniform tokens_per_expert
+        # Generate input data and tokens_per_expert
         num_tokens_per_expert = (bs * sl * top_k * ws) // ne
-        model = GroupedMLP(args).to(torch.bfloat16)
-        x = torch.randn(epr * num_tokens_per_expert, hs, device="cuda", dtype=torch.bfloat16)
-        tokens_per_expert = self.get_uniform_tokens_per_expert(num_tokens_per_expert, epr)
+        total_tokens = experts_per_rank * num_tokens_per_expert
+        x = torch.randn(total_tokens, hs, device="cuda", dtype=torch.bfloat16)
+        tokens_per_expert = self.get_uniform_tokens_per_expert(num_tokens_per_expert, experts_per_rank)
+        assert total_tokens == bs * sl * top_k
+        
+        # Get model and forward/backward function
+        model = dmlp_registry.get(args)
+        if mlp_impl == 'te':
+            forward_func = self.transformerEngineForwardBackward
+        else:
+            model.to(torch.bfloat16)
+            forward_func = self.forwardBackward
 
-        # run benchmark forward backward
-        benchmark = lambda: self.forwardBackward(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=False)
+        # run forward backward benchmark
+        benchmark = lambda: forward_func(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=False)
         mean_t, std_t = benchmark_util.benchmark_function(benchmark)
-        benchmark_util.log_benchmark("3::testGroupedForwardBackwardBenchmark", arguments, mean_t, std_t)
+        benchmark_util.log_benchmark(f"test_{mlp_impl}_forward_backward_benchmark", arguments, mean_t, std_t)
 
-        # run benchmark forward
-        benchmark = lambda: self.forwardBackward(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=True)
+        # run forward benchmark
+        benchmark = lambda: forward_func(model, x, kwargs={"tokens_per_expert": tokens_per_expert}, forward_only=True)
         mean_t, std_t = benchmark_util.benchmark_function(benchmark)
-        benchmark_util.log_benchmark("3::testGroupedForwardBenchmark", arguments, mean_t, std_t)
+        benchmark_util.log_benchmark(f"test_{mlp_impl}_forward_benchmark", arguments, mean_t, std_t)
