12cd3de176dfe62525bf4811f987a9f2b278878e
152334H <54623771+152334H@users.noreply.github.com>
2024-01-08 06:14:30 +0000

    normalize router weights *before* squeezing dim on top-k=1

--- megablocks/layers/router.py
+++ megablocks/layers/router.py
@@ -43,21 +43,16 @@ class LearnedRouter(torch.nn.Module):
         noise = torch.rand(x.size(), dtype=x.dtype, device=x.device)
         return low + noise * (high - low)
 
-    def _top_k(self, scores):
-        if self.args.moe_top_k == 1:
-            return scores.max(dim=-1)
-        return torch.topk(scores, self.args.moe_top_k, dim=-1)
-
-
     def forward(self, x):
         if self.training and self.args.moe_jitter_eps is not None:
             x = x * self.jitter(x)
 
         scores = self.layer(x.view(-1, x.shape[-1])).softmax(dim=-1)
-        expert_weights, expert_indices = self._top_k(scores)
+        expert_weights, expert_indices = torch.topk(scores, self.args.moe_top_k, dim=-1)
         if self.args.moe_normalize_expert_weights:
             expert_weights = expert_weights / torch.norm(
                 expert_weights, p=self.args.moe_normalize_expert_weights,dim=-1, keepdim=True)
+        if self.args.moe_top_k == 1: expert_indices = expert_indices.squeeze(-1)
 
         expert_indices = (
             _uniform_expert_assignment(expert_indices, self.args.moe_num_experts)
