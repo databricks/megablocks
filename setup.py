bb8c4d5082e378a65599d7c55824a62d0263d6c0
Eitan Turok <eitan.turok@databricks.com>
2024-07-19 03:52:04 +0000

    include torch in requires

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ if torch_available:
     cmdclass = {"build_ext": BuildExtension}
 
 install_requires=[
+    "torch",
     "triton>=2.1.0",
     "stanford-stk==0.7.0",
 ]
@@ -59,7 +60,6 @@ extra_deps["dev"] = [
     'pytest-cov>=4,<5',
     'pre-commit>=3.4.0,<4',
     'pytest>=7.2.1,<8',
-    'torch'
 ]
 
 extra_deps['all'] = set(dep for deps in extra_deps.values() for dep in deps)
