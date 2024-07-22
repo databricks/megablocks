cefa7c78ae0afcc0b2cd4fc9ee1e4b8c970a6607
Eitan Turok <eitan.turok@databricks.com>
2024-07-22 00:04:22 +0000

    add torch to install_requires

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ except ModuleNotFoundError as e:
 ###############################################################################
 
 install_requires = [
+    'torch>=2.3.0,<2.4',
     'triton>=2.1.0',
     'stanford-stk==0.7.0',
 ]
