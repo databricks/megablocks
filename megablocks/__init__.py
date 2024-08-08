a309f94a4a1ced01452b021e7e6231725992fc99
Eitan Turok <eitan.turok@databricks.com>
2024-08-08 19:24:22 +0000

    import more files

--- megablocks/__init__.py
+++ megablocks/__init__.py
@@ -1,10 +1,20 @@
 # Copyright 2024 MosaicML MegaBlocks authors
 # SPDX-License-Identifier: Apache-2.0
 
-from megablocks.layers.dmoe import dMoE
-from megablocks.layers.moe import MoE
+from megablocks.layers.arguments import Arguments
+from megablocks.layers.dmoe import ParallelDroplessMLP, dMoE
+from megablocks.layers.glu import SparseGLU
+from megablocks.layers.mlp import MLP, SparseMLP
+from megablocks.layers.moe import MoE, ParallelMLP, get_load_balancing_loss
 
 __all__ = [
     'MoE',
     'dMoE',
+    'get_load_balancing_loss',
+    'ParallelMLP',
+    'ParallelDroplessMLP',
+    'SparseMLP',
+    'MLP',
+    'SparseGLU',
+    'Arguments',
 ]
