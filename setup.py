a1f05a0f0829c7080945b9bce6012a99752ff44c
Eitan Turok <eitan.turok@databricks.com>
2024-08-21 15:20:32 +0000

    torch < 2.4.1

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ classifiers = [
 install_requires = [
     'numpy>=1.21.5,<2.1.0',
     'packaging>=21.3.0,<24.2',
-    'torch>=2.3.0,<2.5',
+    'torch>=2.3.0,<2.4.1',
     'triton>=2.1.0',
     'stanford-stk @ git+https://git@github.com/eitanturok/stk.git@bump-version',
 ]
