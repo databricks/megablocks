a6d613629660fbcd84eabc12f64e2a3e1712b716
Rithwik Ediga Lakhamsani <rithwik.ediga@databricks.com>
2025-03-06 15:50:15 -0800

    upgrading torch to 2.6

--- setup.py
+++ setup.py
@@ -62,8 +62,8 @@ classifiers = [
 install_requires = [
     'numpy>=1.21.5,<2.1.0',
     'packaging>=21.3.0,<24.2',
-    'torch>=2.5.1,<2.5.2',
-    'triton>=2.1.0',
+    'torch>=2.6.0,<2.6.1',
+    'triton>=3.2.0,<3.3.0', # torch 2.6.0 requires ==3.2.0
     'stanford-stk==0.7.1',
 ]
 
